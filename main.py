b4d27ee064aa70a91c7fc746c77f03640ea87e84
Derrick Alvarez <dalvarez@npuins.com>
2023-05-18 22:54:26 -0400

    add init function & progress bar to plot square function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from geopy.distance import geodesic, great_circle
 
@@ -21,6 +22,7 @@ def plot_squares(csv_path, miles):
                       'Bottom Right Y']
     df[corner_columns] = pd.DataFrame([[0] * len(corner_columns)], index=df.index)
 
+    df_length = len(df)
     # iter through rows
     for index, row in df.iterrows():
         coords = (row['LAT'], row['LONG'])
@@ -36,16 +38,32 @@ def plot_squares(csv_path, miles):
         df.at[index, 'Bottom Left Y'] = bottom_left.split(", ")[1]
         df.at[index, 'Bottom Right X'] = bottom_right.split(", ")[0]
         df.at[index, 'Bottom Right Y'] = bottom_right.split(", ")[1]
+
+        # Progress Update
+        progress = (index + 1) / df_length * 100
+        print(f"Processing: {progress:.2f}% complete", end="\r")
         # Uncomment to only process the first ten records.
-        if index > 10:
-            break
+        #if index > 50:
+        #    break
     return df
 
+def init():
+    csv_path = ''
+    while csv_path == '':
+        csv_path = input('Enter the path of the input CSV file: ')
 
-csv_path = '~/Templates/__Misc__/Florida_pt01_gridpoints.csv'
-miles = 10
+    output_path = input('Enter the output path for the modified CSV file (default: root of input CSV): ')
+    if output_path == "":
+        output_path = os.path.join(os.path.dirname(csv_path), "output.csv")
 
-output = plot_squares(csv_path, miles)
+    miles = float(input('Enter the number of miles for the square: '))
 
-output.head().to_csv('~/Templates/__Misc__/output-head.csv', index=False)
-print(output.head())
+    print('Plotting the squares.....')
+    
+    output = plot_squares(csv_path,miles)
+    output.to_csv(output_path, index=False)
+
+if __name__ == '__main__':
+    print('Welcome to the Plot-A-Square 3000')
+    print()
+    init()
